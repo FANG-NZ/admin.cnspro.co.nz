02df92bab68ee271c80744c49bcb9522d123f8d1
Fang (MacPro) <faang1980@gmail.com>
2021-03-23 14:01:00 +1300

    working on projects table styles

--- resources/react/components/projects-table.jsx
+++ resources/react/components/projects-table.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
+import Moment from 'react-moment'
 import {allNewProjects} from '../stores/new-projects-slice'
 
 
@@ -11,15 +12,89 @@ import {allNewProjects} from '../stores/new-projects-slice'
 const ProjectItem = (props) => {
     const _project = props.project
 
+    const date = new Date(_project.completed_on)
 
     return(
-        <React.Fragment>
-            <td>{_project.street},{_project.city}</td>
-            <td>{_project.title}</td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </React.Fragment>
+        <tr>
+            <td className="td-address">
+                <a href="#view" 
+                    onClick={()=>alert("clicked")}
+                >
+                    {_project.street}<br/>{_project.city}
+                </a>
+            </td>
+            <td className="td-title">{_project.title}</td>
+            <td className='td-properties'>
+                <div className="properties-box row">
+                    <div className="col-6">
+                        <span className="properties-item-label">
+                            <i className="mdi mdi-bed-empty"></i>
+                            <span>Bedrooms</span>
+                        </span>
+                        <span className="properties-item-value">
+                            {_project.bedrooms}
+                        </span>
+                    </div>
+                    <div className="col-6">
+                        <span className="properties-item-label">
+                            <i className="mdi mdi-hot-tub"></i>
+                            <span>Bathrooms</span>
+                        </span>
+                        <span className="properties-item-value">
+                            {_project.bathrooms}
+                        </span>
+                    </div>
+
+                    <div className="col-6">
+                        <span className="properties-item-label">
+                            <i className="mdi mdi-car"></i>
+                            <span>Carpark</span>
+                        </span>
+                        <span className="properties-item-value">
+                            {_project.carpark}
+                        </span>
+                    </div>
+                    <div className="col-6">
+                        <span className="properties-item-label">
+                            <i className="mdi mdi-youtube-tv"></i>
+                            <span>Living rooms</span>
+                        </span>
+                        <span className="properties-item-value">
+                            {_project.bathrooms}
+                        </span>
+                    </div>
+
+                    <div className="col-6">
+                        <span className="properties-item-label">
+                            <i className="mdi mdi-grid"></i>
+                            <span>Landarea</span>
+                        </span>
+                        <span className="properties-item-value">
+                            {_project.land_area}
+                        </span>
+                    </div>
+                    <div className="col-6">
+                        <span className="properties-item-label">
+                            <i className="mdi mdi-home"></i>
+                            <span>Floorarea</span>
+                        </span>
+                        <span className="properties-item-value">
+                            {_project.floor_area}
+                        </span>
+                    </div>
+                </div>
+            </td>
+            <td className="td-completed-on">
+                <Moment format="MMM YYYY"> 
+                    {_project.completed_on}
+                </Moment>
+            </td>
+            <td className="td-btns">
+                <button className="btn btn-icon btn-danger btn-sm">
+                    <i className="mdi mdi-close"></i>
+                </button>
+            </td>
+        </tr>
     )
 }
 
@@ -27,36 +102,46 @@ const ProjectItem = (props) => {
  * define the EMPTY project item
  * @returns 
  */
-const EmptyProjectItem = () => {
-
+const EmptyProjectItem = (props) => {
+    
     return(
+        <tr>
         <td colSpan="5">
             <div className="alert alert-warning mb-0">
-                There is <strong>NO</strong> any new projects added
+                {props.message}
             </div>
         </td>
+        </tr>
     )
 }
 
 
-
+/**
+ * define the projects table 
+ * @returns 
+ */
 const ProjectsTable = () => {
     const _projects = useSelector(allNewProjects)
 
+    //define the warning message
+    const _message = <>There is <strong>NO</strong> any new projects added</>
+
     return(
         <React.Fragment>
-            <table className="table mb-0">
+            <table className="table mb-0 projects-table">
                 <thead>
-                    <th>Address</th>
-                    <th>Title</th>
-                    <th>Properties</th>
-                    <th>Completed On</th>
-                    <th></th>
+                    <tr>
+                        <th>Address</th>
+                        <th>Title</th>
+                        <th>Properties</th>
+                        <th>Completed On</th>
+                        <th className="th-btns"></th>
+                    </tr>
                 </thead>
 
                 <tbody>
                     {_projects.length === 0
-                     ?  <EmptyProjectItem />
+                     ?  <EmptyProjectItem message={_message} />
                      :  <>
                             {_projects.map((item) => {
                                 return <ProjectItem key={item.id} project={item} />
--- resources/react/pages/new-projects-page.js
+++ resources/react/pages/new-projects-page.js
@@ -5,6 +5,19 @@ import {Provider} from 'react-redux'
 import store from '../stores/new-projects-page-store'
 import ProjectsTable from '../components/projects-table'
 
+import {setNewProjects} from '../stores/new-projects-slice'
+
+//To get json string from DOM
+const json_string = document.getElementById("root-new-projects").getAttribute("projects-data");
+if(!json_string){
+    throw new Error("NOT FOUND JSON STRING")
+}
+
+//Try to load data from DOM attribute,
+//if there is NO data, we just send AJAX request to fetch from server
+const _data = JSON.parse(json_string)
+store.dispatch(setNewProjects(_data))
+
 ReactDom.render(
     <Provider store={store}>
         <h4 className="m-t-0 header-title">New projects list</h4>
@@ -13,4 +26,3 @@ ReactDom.render(
     , 
     document.getElementById("root-new-projects")
 )
-
