1bc8ae122bc8760f6d5c7795b2394acf3f8a5147
Fang (MacPro) <faang1980@gmail.com>
2021-04-21 21:34:45 +1200

    done fix import PNG into testing

--- mocks/jest-file-mock.js
+++ mocks/jest-file-mock.js
@@ -1,3 +1,4 @@
 //This is to fix import IMAGE FILE error in JEST TESTING
 //https://stackoverflow.com/questions/46898638/importing-images-breaks-jest-test
-export default ''
+//module.exports = 'test-file-mock'; 
+export default 'test-file-mock';
